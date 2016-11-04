95f4f0366f3a14049e2980ebdf266906acd504c6
Remco <remco@dutchcoders.io>
2016-11-04 09:38:06 +0100

    fix drawing selected nodes

--- src/app/components/Graphs/Graph.js
+++ src/app/components/Graphs/Graph.js
@@ -253,11 +253,15 @@ class Graph extends React.Component {
                     this.context.fillStyle = color;
                     this.context.fill();
 
-                    if (includes(this.graph.selectedNodes, d)) {
-                        this.context.strokeStyle = '#993833';
-                        this.context.lineWidth = this.nodes.stroke.thickness;
-                        this.context.stroke();
-                    }
+                }
+
+                this.context.beginPath();
+                this.context.arc(d.x, d.y, d.r, 0, 2 * Math.PI);
+
+                if (includes(this.graph.selectedNodes, d)) {
+                    this.context.strokeStyle = '#993833';
+                    this.context.lineWidth = this.nodes.stroke.thickness;
+                    this.context.stroke();
                 }
 
                 // this.context.font="14px FontAwesome";
