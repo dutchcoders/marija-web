16db9f4184dd53cc885e9dbcfac9eb939ba46c00
Thomas Kuipers <thomas@webvalid.nl>
2017-12-20 22:53:58 +0200

    via test

--- src/app/helpers/getNodesAndLinks.test.js
+++ src/app/helpers/getNodesAndLinks.test.js
@@ -100,6 +100,12 @@ test('should output links between related nodes', () => {
     expect(links.length).toBe(1);
 });
 
+// test if a link exists between a source and a target
+const expectLink = (links, source, target) => {
+    const link = links.find(link => link.source === source && link.target === target);
+    expect(link).toBeDefined();
+};
+
 test('when nodes have exactly the same fields they should not be duplicated', () => {
     const previousNodes = [];
     const previousLinks = [];
@@ -118,4 +124,77 @@ test('when nodes have exactly the same fields they should not be duplicated', ()
     const { nodes } = getNodesAndLinks(previousNodes, previousLinks, items, fields, query, normalizations);
 
     expect(nodes.length).toBe(1);
+});
+
+test('should do via stuff', () => {
+    const previousNodes = [];
+    const previousLinks = [];
+    const normalizations = [];
+
+    const items = [
+        generateItem({
+            'src-ip': 1,
+            'dst-net': null,
+            'src-ip_dst-net_port': '1_2_80'
+        }),
+        generateItem({
+            'src-ip': null,
+            'dst-net': 2,
+            'src-ip_dst-net_port': '1_2_80'
+        })
+    ];
+
+    const fields = [
+        generateField('src-ip'),
+        generateField('dst-net'),
+        generateField('src-ip_dst-net_port')
+    ];
+
+    const query = generateQuery(items);
+    const { nodes, links } = getNodesAndLinks(previousNodes, previousLinks, items, fields, query, normalizations);
+
+    expect(links).toBeDefined();
+    expect(links.length).toBe(2);
+
+    console.log(links);
+
+    expectLink(links, 1, '1_2_80');
+    expectLink(links, 2, '1_2_80');
+});
+
+test('should do via stuff 2', () => {
+    const previousNodes = [];
+    const previousLinks = [];
+    const normalizations = [];
+
+    const items = [
+        generateItem({
+            'src-ip': 1,
+            'dst-net': null,
+            'source_dest_port': '1_2_80'
+        }),
+        generateItem({
+            'src-ip': null,
+            'dst-net': 2,
+            'source_dest_port': '1_2_80'
+        })
+    ];
+
+    const fields = [
+        generateField('src-ip'),
+        generateField('dst-net'),
+        generateField('source_dest_port')
+    ];
+
+    const query = generateQuery(items);
+    const { nodes, links } = getNodesAndLinks(previousNodes, previousLinks, items, fields, query, normalizations);
+
+    expect(links).toBeDefined();
+    expect(links.length).toBe(2);
+
+    console.log(nodes);
+    console.log(links);
+
+    expectLink(links, 1, '1_2_80');
+    expectLink(links, 2, '1_2_80');
 });
