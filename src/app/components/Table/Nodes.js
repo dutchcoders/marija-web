effb2fe09bfed359e8b6a65ab7820c0f369ab4e1
Thomas Kuipers <thomas@webvalid.nl>
2018-01-18 11:04:46 +0200

    change wording in nodes pane

--- src/app/components/Table/Nodes.js
+++ src/app/components/Table/Nodes.js
@@ -242,12 +242,12 @@ class Nodes extends React.Component {
         return (
             <div className="form-group toolbar">
                 <div className="nodes-btn-group" role="group">
-                    <button type="button" className="btn btn-default" aria-label="Clear selection" onClick={() => this.handleClearSelection()}>clear</button>
+                    <button type="button" className="btn btn-default" aria-label="Clear selection" onClick={() => this.handleClearSelection()}>deselect</button>
                     <button type="button" className="btn btn-default" aria-label="Select related nodes" onClick={() => this.handleSelectRelatedNodes()}>related</button>
                     <button type="button" className="btn btn-default" aria-label="Find nodes" onClick={() => this.handleFindNodes()}>find</button>
                     <button type="button" className="btn btn-default" aria-label="Select all nodes" onClick={() => this.handleSelectAllNodes()}>all</button>
                     <button type="button" className="btn btn-default" aria-label="Delete selected nodes" onClick={() => this.handleDeleteAllNodes()}>delete</button>
-                    <button type="button" className="btn btn-default" aria-label="Delete but selected nodes" onClick={() => this.handleDeleteAllButSelectedNodes()}>inverse delete</button>
+                    <button type="button" className="btn btn-default" aria-label="Delete but selected nodes" onClick={() => this.handleDeleteAllButSelectedNodes()}>delete others</button>
                 </div>
                 <div>
                     <ul onMouseLeave={this.hideTooltip.bind(this)}>
