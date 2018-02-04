8f83b89df4ecfcaa00874211aef8e5fa10281a69
Remco Verhoef <remco@dutchcoders.io>
2018-02-04 23:43:42 +0100

    disabled support for complex objects, all fields are flattened now

--- src/app/helpers/fieldLocator.js
+++ src/app/helpers/fieldLocator.js
@@ -1,6 +1,7 @@
 export default function fieldLocator(document, field) {
     if (!field) {return false;}
 
+    /*
     const field_levels = field.split('.');
 
     const value = field_levels.reduce((currentLevelInDocument, currentField) => {
@@ -14,6 +15,7 @@ export default function fieldLocator(document, field) {
 
         return null;
     }, document);
+    */
 
-    return (value);
+    return (document[field]);
 }
