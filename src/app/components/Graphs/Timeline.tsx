2987fbefde1eb1428e40a396ac6fae3915b49189
Thomas Kuipers <thomas@webvalid.nl>
2018-02-25 16:26:14 +0200

    render timeline chart with recharts

--- src/app/components/Graphs/Timeline.tsx
+++ src/app/components/Graphs/Timeline.tsx
@@ -13,6 +13,8 @@ import {Moment} from "moment";
 import {FormEvent} from "react";
 import {dateFieldAdd, dateFieldDelete} from '../../modules/data/actions';
 import {searchFieldsUpdate} from '../../modules/search/actions';
+import {BarChart, XAxis, YAxis, Bar} from 'recharts';
+import {Search} from "../../interfaces/search";
 
 interface Props {
     normalizations: Normalization[];
@@ -24,6 +26,7 @@ interface Props {
     containerWidth: number;
     containerHeight: number;
     dispatch: Dispatch<any>;
+    searches: Search[];
 }
 
 interface State {
@@ -31,212 +34,9 @@ interface State {
 }
 
 class Timeline extends React.Component<Props, State> {
-    canvas: HTMLCanvasElement;
-    context: CanvasRenderingContext2D;
     state: State = {
         showAllFields: false
     };
-
-    constructor(props) {
-        super(props);
-
-        this.draw = this.draw.bind(this);
-    }
-
-    componentDidMount() {
-        this.canvas = this.refs.canvas as HTMLCanvasElement;
-        this.context = this.canvas.getContext('2d');
-        this.draw();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.debug("componentDidUpdate (histogram");
-        // group items to periods using lodash? complete set
-        // have selection filter and drag timeline to select nodes
-        //
-
-        this.draw();
-    }
-
-    draw() {
-        const { normalizations, fields, date_fields, items, selectedNodes } = this.props;
-        const { canvas } = this;
-
-        if (!items.length || !date_fields.length) {
-            return;
-        }
-
-        let itemsCopy = concat([], items);
-
-        const context = this.context;
-        context.save();
-
-        var margin = {top: 20, right: 20, bottom: 30, left: 40},
-            width = canvas.width - margin.left - margin.right,
-            height = canvas.height - margin.top - margin.bottom;
-
-        this.context.clearRect(0, 0, canvas.width, canvas.height);
-
-        const x = d3.scaleBand()
-            .rangeRound([0, width])
-            .padding(0.1);
-
-        const y = d3.scaleLinear()
-            .rangeRound([height, 0]);
-
-        context.translate(margin.left, margin.top);
-
-        const groupedResults = groupBy(itemsCopy, (result) => {
-            for (var date_field of date_fields) {
-                let date = fieldLocator(result.fields, date_field.path);
-                if (!date) {
-                    continue;
-                }
-
-                return moment(date).year() + '-' + moment(date).month();
-            }
-        });
-
-        let minX = moment();
-
-        items.forEach(item => {
-            date_fields.forEach(field => {
-                const date: any = fieldLocator(item.fields, field.path);
-
-                if (!date) {
-                    return;
-                }
-
-                const parsed: Moment = moment(date);
-
-                if (parsed < minX) {
-                    minX = parsed;
-                }
-            });
-        });
-
-        let maxX = moment();
-
-        items.forEach(item => {
-            date_fields.forEach(field => {
-                const date: any = fieldLocator(item.fields, field.path);
-
-                if (!date) {
-                    return;
-                }
-
-                const parsed: Moment = moment(date);
-
-                if (parsed > maxX) {
-                    maxX = parsed;
-                }
-            });
-        });
-
-        const periods = [];
-
-        let year = minX.year();
-        let month = minX.month();
-        for (; year < maxX.year() || (year == maxX.year() && month < maxX.month());) {
-            month++;
-            if (month > 12) {
-                year++;
-                month = 1;
-            }
-
-            periods.push(year + "-" + month);
-        }
-
-        x.domain(periods);
-
-        const maxValue = reduce(groupedResults, (max, n, m) => (n.length > max ? n.length : max), 0);
-        y.domain([0, maxValue]);
-
-        const yTickCount = 10,
-            yTicks = y.ticks(yTickCount),
-            yTickFormat = y.tickFormat(yTickCount);
-
-        context.beginPath();
-        x.domain().forEach((d) => {
-            context.moveTo(x(d) + x.bandwidth() / 2, height);
-            context.lineTo(x(d) + x.bandwidth() / 2, height + 6);
-        });
-
-        // todo(nl5887): add selection
-        // todo(nl5887): show difference between queries
-
-        context.strokeStyle = "#b5b5b5";
-        context.stroke();
-
-        context.textAlign = "center";
-        context.fillStyle = "#b5b5b5";
-        context.textBaseline = "top";
-        x.domain().forEach((d) => {
-            context.fillText(d, x(d) + x.bandwidth() / 2, height + 6);
-        });
-
-        context.beginPath();
-        yTicks.forEach((d) => {
-            context.moveTo(0, y(d) + 0.5);
-            context.lineTo(-6, y(d) + 0.5);
-        });
-        context.strokeStyle = "#b5b5b5";
-        context.stroke();
-
-        context.textAlign = "right";
-        context.textBaseline = "middle";
-        context.fillStyle = "#b5b5b5";
-        yTicks.forEach((d) => {
-            context.fillText(yTickFormat(d), -9, y(d));
-        });
-
-        context.beginPath();
-        context.moveTo(-6.5, 0 + 0.5);
-        context.lineTo(0.5, 0 + 0.5);
-        context.lineTo(0.5, height + 0.5);
-        context.lineTo(-6.5, height + 0.5);
-        context.strokeStyle = "#b5b5b5";
-        context.stroke();
-
-        context.fillStyle = "#57c17b";
-
-        forEach(groupedResults, (d, v) => {
-            context.fillRect(x(v), y(d.length), x.bandwidth(), height - y(d.length));
-        });
-
-        if (selectedNodes.length > 0) {
-            itemsCopy = filter(itemsCopy, (item) => {
-                // check if node contains item
-                return reduce(fields, (found, field) => {
-                    const val = fieldLocator(item.fields, field.path);
-                    if (!val) {return found;}
-
-                    return found || find(selectedNodes, (o) => {
-                        return (o.id === normalize(normalizations, val));
-                    });
-                }, false);
-            });
-
-            const groupedResultsSelection = groupBy(itemsCopy, (result) => {
-                for (var date_field of date_fields) {
-                    let date = fieldLocator(result.fields, date_field.path);
-                    if (!date) {
-                        continue;
-                    }
-
-                    return moment(date).year() + '-' + moment(date).month();
-                }
-            });
-
-            context.fillStyle = "#fac04b";
-            forEach(groupedResultsSelection, (d, v) => {
-                context.fillRect(x(v), y(d.length), x.bandwidth(), height - y(d.length));
-            });
-        }
-
-
-        context.restore();
-    }
 
     handleFieldChange(event: FormEvent<HTMLInputElement>, field: Field) {
         const { dispatch } = this.props;
@@ -302,8 +102,95 @@ class Timeline extends React.Component<Props, State> {
         });
     }
 
+    getChart() {
+        const { date_fields, items, containerHeight, containerWidth } = this.props;
+
+        if (!items.length || !date_fields.length) {
+            return;
+        }
+
+        const times: Moment[] = [];
+
+        const groupedResults = groupBy(items, (result) => {
+            for (var date_field of date_fields) {
+                let date = fieldLocator(result.fields, date_field.path);
+                if (!date) {
+                    continue;
+                }
+
+                const parsed = moment(date);
+                times.push(parsed);
+
+                return parsed.year() + '-' + (parsed.month() + 1);
+            }
+        });
+
+        times.sort((a: Moment, b: Moment) => {
+            return a.unix() - b.unix();
+        });
+
+        const periods: string[] = [];
+
+        times.forEach(moment => {
+            const string: string = moment.year() + '-' + (moment.month() + 1);
+
+            if (periods.indexOf(string) === -1) {
+                periods.push(string);
+            }
+        });
+
+        const queries: string[] = items.reduce((previous, item: Item) => {
+            if (previous.indexOf(item.query) === -1) {
+                return previous.concat([item.query]);
+            }
+
+            return previous;
+        }, []);
+
+        const chartData = periods.map(period => {
+            const data = {
+                name: period
+            };
+
+            queries.forEach(query => {
+
+
+                data[query] = groupedResults[period].filter(item => item.query === query).length
+            });
+
+            return data;
+        });
+
+        return (
+            <BarChart
+                width={containerWidth}
+                height={containerHeight - 30}
+                margin={{top: 0, right: 0, bottom: 0, left: 0}}
+                data={chartData}>
+                <XAxis dataKey="name" stroke="white"/>
+                <YAxis stroke="white" />
+                {queries.map(query =>
+                    <Bar
+                        key={query}
+                        dataKey={query}
+                        stackId="a" 
+                        fill={this.getQueryColor(query)}
+                    />
+                )}
+            </BarChart>
+        );
+    }
+
+    getQueryColor(query: string): string {
+        const { searches } = this.props;
+
+        const search = searches.find(searchLoop => searchLoop.q === query);
+
+        return search.color;
+    }
+
     render() {
-        const { items, containerHeight, containerWidth, date_fields } = this.props;
+        const { items, date_fields } = this.props;
 
         let noitems = null; 
         if (items.length === 0) {
@@ -317,19 +204,17 @@ class Timeline extends React.Component<Props, State> {
             );
         }
 
-        const showCanvas: boolean = !noDateFields && !noitems;
+        let chart = null;
+        if (!noDateFields && !noitems) {
+            chart = this.getChart();
+        }
 
         return (
             <div>
                 { this.selectDateFields() }
                 { noitems }
                 { noDateFields }
-                <canvas
-                    className={showCanvas ? '' : 'hidden'}
-                    width={ containerWidth }
-                    height={ containerHeight - 30 }
-                    ref="canvas"
-                />
+                { chart }
             </div>
         );
 
@@ -345,7 +230,8 @@ const select = (state, ownProps) => {
         fields: state.entries.fields,
         normalizations: state.entries.normalizations,
         date_fields: state.entries.date_fields,
-        items: state.entries.items
+        items: state.entries.items,
+        searches: state.entries.searches
     };
 };
 
