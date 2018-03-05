776cf1dbf3e1bd6f38248a82d92d8d412ca14eb3
Thomas Kuipers <thomas@webvalid.nl>
2018-03-05 14:20:28 +0200

    fix image loading

--- webpack.common.js
+++ webpack.common.js
@@ -50,7 +50,7 @@ module.exports = {
                 loader: "source-map-loader"
             },
             {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                test: /\.(woff|woff2|eot|ttf|svg)$/,
                 loader: 'url-loader?limit=100000'
             },
             {
