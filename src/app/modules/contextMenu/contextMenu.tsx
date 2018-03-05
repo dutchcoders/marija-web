5a3fd9dc5f50d00c5838b725e65696f9c159bd80
Thomas Kuipers <thomas@webvalid.nl>
2018-03-05 18:19:14 +0200

    rename nodes from context menu

--- src/app/modules/contextMenu/contextMenu.tsx
+++ src/app/modules/contextMenu/contextMenu.tsx
@@ -3,11 +3,13 @@ import {Node} from "../../interfaces/node";
 import * as styles from './contextMenu.scss';
 import { Icon } from '../../components/index';
 import {connect, Dispatch} from "react-redux";
-import {deleteNodes, nodesSelect} from "../graph";
+import {deleteNodes, nodesSelect, nodeUpdate} from "../graph";
 import getDirectlyRelatedNodes from "../../helpers/getDirectlyRelatedNodes";
 import {Link} from "../../interfaces/link";
 import {hideContextMenu} from "./contextMenuActions";
 import {searchAround} from "../search/actions";
+import {FormEvent} from "react";
+import abbreviateNodeName from '../../helpers/abbreviateNodeName';
 
 interface Props {
     node: Node;
@@ -19,10 +21,29 @@ interface Props {
 }
 
 interface State {
-
+    renameOpened: boolean;
+    renameTo: string;
 }
 
 class ContextMenu extends React.Component<Props, State> {
+    renameInput: HTMLInputElement;
+
+    state: State = {
+        renameOpened: false,
+        renameTo: ''
+    };
+
+    componentWillReceiveProps(nextProps: Props) {
+        if (!nextProps.node) {
+            return;
+        }
+
+        this.setState({
+            renameTo: nextProps.node.name,
+            renameOpened: false
+        });
+    }
+
     selectRelated() {
         const { dispatch, nodes, links, node } = this.props;
 
@@ -51,28 +72,100 @@ class ContextMenu extends React.Component<Props, State> {
         dispatch(hideContextMenu());
     }
 
+    openRename() {
+        this.setState({
+            renameOpened: true
+        });
+    }
+
+    handleRenameChange(event: FormEvent<HTMLInputElement>) {
+        this.setState({
+            renameTo: event.currentTarget.value
+        });
+    }
+
+    handleRenameEvents(event: KeyboardEvent) {
+        const { dispatch, node } = this.props;
+        const { renameTo } = this.state;
+
+        if (event.key === 'Enter') {
+            dispatch(nodeUpdate(node.id, {
+                name: renameTo,
+                abbreviated: abbreviateNodeName(renameTo, node.queries[0], 20)
+            }));
+
+            this.close();
+        } else if (event.key === 'Escape') {
+            this.setState({
+                renameOpened: false,
+                renameTo: node.name
+            });
+        }
+    }
+
+    handleRenameBlur() {
+        this.setState({
+            renameOpened: false
+        });
+    }
+
     render() {
         const { node, x, y } = this.props;
+        const { renameOpened, renameTo } = this.state;
 
         if (!node) {
             return null;
         }
 
+        let rename;
+
+        if (renameOpened) {
+            rename = (
+                <div onClick={this.openRename.bind(this)} className={styles.inputWrapper}>
+                    <Icon name={'ion-ios-compose ' + styles.icon} />
+                    <input
+                        autoFocus={true}
+                        onChange={this.handleRenameChange.bind(this)}
+                        onKeyDown={this.handleRenameEvents.bind(this)}
+                        onBlur={this.handleRenameBlur.bind(this)}
+                        ref={element => this.renameInput = element}
+                        value={renameTo}
+                    />
+                </div>
+            );
+        } else {
+            rename = (
+                <button onClick={this.openRename.bind(this)} className={styles.button}>
+                    <Icon name={'ion-ios-compose ' + styles.icon} />
+                    <span className={styles.title}>Rename</span>
+                </button>
+            );
+        }
+
         return (
-            <nav className={styles.contextMenu} style={{top: y, left: x}}>
-                <button onClick={this.selectRelated.bind(this)} className={styles.button}>
-                    <Icon name={'ion-qr-scanner ' + styles.icon} />
-                    <span className={styles.title}>Select related</span>
-                </button>
-                <button onClick={this.searchAround.bind(this)} className={styles.button}>
-                    <Icon name={'ion-ios-search ' + styles.icon} />
-                    <span className={styles.title}>Search around</span>
-                </button>
-                <button onClick={this.delete.bind(this)} className={styles.button}>
-                    <Icon name={'ion-ios-trash ' + styles.icon} />
-                    <span className={styles.title}>Delete</span>
-                </button>
-            </nav>
+            <ul className={styles.contextMenu} style={{top: y, left: x}}>
+                <li>
+                    <button onClick={this.selectRelated.bind(this)} className={styles.button}>
+                        <Icon name={'ion-qr-scanner ' + styles.icon} />
+                        <span className={styles.title}>Select related</span>
+                    </button>
+                </li>
+                <li>
+                    <button onClick={this.searchAround.bind(this)} className={styles.button}>
+                        <Icon name={'ion-ios-search ' + styles.icon} />
+                        <span className={styles.title}>Search around</span>
+                    </button>
+                </li>
+                <li>
+                    {rename}
+                </li>
+                <li>
+                    <button onClick={this.delete.bind(this)} className={styles.button}>
+                        <Icon name={'ion-ios-trash ' + styles.icon} />
+                        <span className={styles.title}>Delete</span>
+                    </button>
+                </li>
+            </ul>
         );
     }
 }
