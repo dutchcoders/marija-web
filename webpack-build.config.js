272ad9b55b8523700bbe4c23d83992285d73788a
Thomas Kuipers <thomas@webvalid.nl>
2018-03-07 20:11:11 +0200

    add html webpack plugin:

--- webpack-build.config.js
+++ webpack-build.config.js
@@ -1,9 +1,13 @@
-const baseConfig = require('./webpack.config');
+const baseConfig = require('./webpack.common.config');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = Object.assign(baseConfig, {
     output: {
         path: [__dirname, 'dist'].join(path.sep),
         filename: 'bundle.js'
     },
+    plugins: baseConfig.plugins.concat([
+        new HtmlWebpackPlugin()
+    ])
 });
