34be22986b981f843f424894284977c56be3a3ed
Thomas Kuipers <thomas@webvalid.nl>
2018-04-05 16:28:57 +0200

    remove unused batch module

--- src/app/modules/batch/actions.js
+++
@@ -1,7 +0,0 @@
-export function batchActions(...actions) {
-  console.debug("batchActions", actions);
-  return {
-    type: 'BATCH_ACTIONS',
-    actions: actions
-  };
-}
--- src/app/modules/batch/index.js
+++
@@ -1,2 +0,0 @@
-export { batchActions } from './actions';
-
