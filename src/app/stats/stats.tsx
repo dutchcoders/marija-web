4afa190053ee874328313d4730907064cd1f16ee
Thomas Kuipers <thomas@webvalid.nl>
2018-06-14 16:45:18 +0200

    display number of nodes and links in stats

--- src/app/stats/stats.tsx
+++ src/app/stats/stats.tsx
@@ -2,10 +2,18 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import { AppState } from '../main/interfaces/appState';
+import { Node } from '../graph/interfaces/node';
+import { Link } from '../graph/interfaces/link';
+import {
+	getLinksForDisplay,
+	getNodesForDisplay
+} from '../graph/graphSelectors';
 
 interface Props {
     serverVersion: string;
     fps: number;
+    nodesForDisplay: Node[];
+    linksForDisplay: Link[];
 }
 
 interface State {
@@ -13,12 +21,12 @@ interface State {
 
 class Stats extends React.Component<Props, State> {
     render() {
-        const { serverVersion, fps } = this.props;
+        const { serverVersion, fps, nodesForDisplay, linksForDisplay } = this.props;
         const clientVersion = process.env.CLIENT_VERSION;
 
         return (
             <p className="stats">
-                {fps.toFixed(1)}<br />
+                {fps.toFixed(1)} FPS {nodesForDisplay.length} nodes {linksForDisplay.length} links<br />
                 SERVER VERSION: {serverVersion}<br />
                 CLIENT VERSION: {clientVersion}
             </p>
@@ -30,7 +38,9 @@ class Stats extends React.Component<Props, State> {
 const select = (state: AppState, ownProps) => {
     return {
         serverVersion: state.stats.serverVersion,
-        fps: state.stats.fps
+        fps: state.stats.fps,
+        nodesForDisplay: getNodesForDisplay(state),
+        linksForDisplay: getLinksForDisplay(state),
     };
 };
 
