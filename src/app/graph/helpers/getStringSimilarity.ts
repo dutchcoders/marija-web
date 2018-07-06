b3839ffd3f001927de984798e652ffc013ccd141
Thomas Kuipers <thomas@webvalid.nl>
2018-07-06 13:27:46 +0200

    fix connector bugs

--- src/app/graph/helpers/getStringSimilarity.ts
+++ src/app/graph/helpers/getStringSimilarity.ts
@@ -2,7 +2,8 @@ import { get } from 'fast-levenshtein';
 
 export function getStringSimilarity(a: string, b: string): number {
 	const distance = get(a, b);
-	const fraction = 1 - distance / a.length;
+	const longestLength = Math.max(a.length, b.length);
+	const fraction = 1 - distance / longestLength;
 
 	return fraction * 100;
 }
