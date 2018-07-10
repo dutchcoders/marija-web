2b29864e258c3e01e8f5e8f37de8e10fb7097d34
Thomas Kuipers <thomas@webvalid.nl>
2018-07-10 09:45:46 +0200

    fix socket url

--- src/app/connection/helpers/webSocketMiddleware.ts
+++ src/app/connection/helpers/webSocketMiddleware.ts
@@ -132,19 +132,6 @@ function onClose(event: CloseEvent, dispatch: Dispatch<any>) {
     dispatch(authConnected(false, reason));
 }
 
-function getUrl(): string {
-    let url;
-
-    if (process.env.WEBSOCKET_URI) {
-        url = process.env.WEBSOCKET_URI;
-    } else {
-        const {location} = window;
-        url = ((location.protocol === "https:") ? "wss://" : "ws://") + location.host + "/ws";
-    }
-
-    return url;
-}
-
 function getCloseMessage(event: CloseEvent): string {
     switch (event.code) {
         case 1000:
--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -2,6 +2,19 @@ import * as React from 'react';
 import {render} from "react-dom";
 import {Marija} from '../app';
 
+function getUrl(): string {
+	let url;
+
+	if (process.env.WEBSOCKET_URI) {
+		url = process.env.WEBSOCKET_URI;
+	} else {
+		const {location} = window;
+		url = ((location.protocol === "https:") ? "wss://" : "ws://") + location.host + "/ws";
+	}
+
+	return url;
+}
+
 render((
-    <Marija backendUri={process.env.WEBSOCKET_URI} />
+    <Marija backendUri={getUrl()} />
 ), document.getElementById('root'));
