17c96c4beedc590afeb7a977d7fa4ba0e7fcf2bf
Thomas Kuipers <thomas@webvalid.nl>
2018-07-16 12:22:59 +0200

    display total nodes and links

--- src/app/stats/stats.tsx
+++ src/app/stats/stats.tsx
@@ -13,6 +13,8 @@ interface Props {
     fps: number;
     nodesForDisplay: Node[];
     linksForDisplay: Link[];
+    nodes: Node[];
+    links: Link[];
 }
 
 interface State {
@@ -20,11 +22,13 @@ interface State {
 
 class Stats extends React.Component<Props, State> {
     render() {
-        const { fps, nodesForDisplay, linksForDisplay } = this.props;
+        const { fps, nodesForDisplay, linksForDisplay, nodes, links } = this.props;
 
         return (
             <p className="stats">
-                {fps.toFixed(1)} FPS {nodesForDisplay.length} nodes {linksForDisplay.length} links<br />
+                {fps.toFixed(1)} FPS<br />
+				{nodesForDisplay.length}/{nodes.length} nodes<br />
+				{linksForDisplay.length}/{links.length} links
             </p>
         );
     }
@@ -34,6 +38,8 @@ class Stats extends React.Component<Props, State> {
 const select = (state: AppState, ownProps) => {
     return {
         fps: state.stats.fps,
+		nodes: state.graph.nodes,
+		links: state.graph.links,
         nodesForDisplay: getNodesForDisplay(state),
         linksForDisplay: getLinksForDisplay(state),
     };
