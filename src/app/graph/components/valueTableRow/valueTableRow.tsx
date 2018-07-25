707d6d300d3c3a748bbb1d02a3a35f6dc74b1aa1
Thomas Kuipers <thomas@webvalid.nl>
2018-07-25 14:07:06 +0200

    select button in value table

--- src/app/graph/components/valueTableRow/valueTableRow.tsx
+++ src/app/graph/components/valueTableRow/valueTableRow.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { ValueInfo } from '../../helpers/getValueInfo';
 import * as styles from './valueTableRow.scss';
+import { nodesSelect } from '../../graphActions';
+import { connect } from 'react-redux';
 
 interface Props {
 	valueInfo: ValueInfo;
+	dispatch: any;
 }
 
 class ValueTableRow extends React.Component<Props> {
+	selectNodes() {
+		const { valueInfo, dispatch } = this.props;
+
+		dispatch(nodesSelect(valueInfo.nodes));
+	}
+
 	render() {
 		const { valueInfo } = this.props;
 
@@ -15,10 +24,12 @@ class ValueTableRow extends React.Component<Props> {
 				<td className={styles.td}>{valueInfo.value.substring(0, 100) + (valueInfo.value.length > 100 ? '...' : '')}</td>
 				<td className={styles.td + ' ' + styles.number}>{valueInfo.occurences}</td>
 				<td className={styles.td}>{valueInfo.fields.join(', ')}</td>
-				<td className={styles.td}>{valueInfo.nodes.length}</td>
+				<td className={styles.td}>
+					<button onClick={this.selectNodes.bind(this)} className={styles.select}>Select {valueInfo.nodes.length}</button>
+				</td>
 			</tr>
 		);
 	}
 }
 
-export default ValueTableRow;
+export default connect()(ValueTableRow);
