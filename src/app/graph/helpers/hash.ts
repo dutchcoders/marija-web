721dafe5239eea663d687218d20597785f24c7b4
Thomas Kuipers <thomas@webvalid.nl>
2018-07-26 10:16:35 +0200

    remove unused function

--- src/app/graph/helpers/hash.ts
+++
@@ -1,7 +0,0 @@
-export default function hash(str) {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    return hash;
-}
