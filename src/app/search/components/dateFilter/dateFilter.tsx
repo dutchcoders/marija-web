298ffbd9da1259a2f18cf364d381bbdd8833b43b
Thomas Kuipers <thomas@webvalid.nl>
2018-08-06 15:54:36 +0200

    translate dateFilter

--- src/app/search/components/dateFilter/dateFilter.tsx
+++ src/app/search/components/dateFilter/dateFilter.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as styles from './dateFilter.scss';
 import { FormEvent } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 interface Props {
 	date: string;
@@ -19,22 +20,22 @@ class DateFilter extends React.Component<Props> {
 
 		return (
 			<div className={styles.dates}>
-				<h3 className={styles.datesTitle}>Maximum age</h3>
+				<h3 className={styles.datesTitle}><FormattedMessage id="maximum_age"/></h3>
 				<label className={styles.dateLabel}>
 					<input type="radio" name="maxAge" value="" className={styles.dateRadio} checked={date === ''} onChange={this.onChange.bind(this)}/>
-					<span>All</span>
+					<span><FormattedMessage id="all_time"/></span>
 				</label>
 				<label className={styles.dateLabel}>
 					<input type="radio" name="maxAge" value="1month" className={styles.dateRadio} checked={date === '1month'} onChange={this.onChange.bind(this)}/>
-					<span>1 Month</span>
+					<span><FormattedMessage id="1_month"/></span>
 				</label>
 				<label className={styles.dateLabel}>
 					<input type="radio" name="maxAge" value="1week" className={styles.dateRadio} checked={date === '1week'} onChange={this.onChange.bind(this)}/>
-					<span>1 Week</span>
+					<span><FormattedMessage id="1_week"/></span>
 				</label>
 				<label className={styles.dateLabel}>
 					<input type="radio" name="maxAge" value="1day" className={styles.dateRadio} checked={date === '1day'} onChange={this.onChange.bind(this)}/>
-					<span>1 Day</span>
+					<span><FormattedMessage id="1_day"/></span>
 				</label>
 			</div>
 		)
