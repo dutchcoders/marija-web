759ff951813709191e24b2bdd362ccfa4ccb8b3d
Thomas Kuipers <thomas@webvalid.nl>
2018-08-07 16:38:29 +0200

    improve field search styling

--- src/app/fields/fields.tsx
+++ src/app/fields/fields.tsx
@@ -14,6 +14,7 @@ import {
 import FieldList from './components/fieldList/fieldList';
 import { selectDatasourcesInData } from '../datasources/datasourcesSelectors';
 import { FormattedMessage, InjectedIntl, injectIntl } from 'react-intl';
+import Icon from '../ui/components/icon';
 
 interface Props {
     dispatch: Dispatch<any>;
@@ -58,32 +59,23 @@ class Fields extends React.Component<Props, State> {
         const { searchTypes } = this.state;
 
         return (
-            <div className={'row ' + styles.filter}>
-                <div className="col-xs-12">
-                    <div className={styles.filterContent}>
-                        {typeLabels.map(type => {
-                            const key = 'search_types_' + type.types.join(',');
+            <div className={styles.filter}>
+				{typeLabels.map(type => {
+					const key = 'search_types_' + type.types.join(',');
 
-                            return (
-                                <div className="form-check form-check-inline" key={key}>
-                                    <input
-                                        type="radio"
-                                        className="form-check-input"
-                                        name="type"
-                                        id={key}
-                                        checked={isEqual(type.types, searchTypes)}
-                                        onChange={(e) => this.handleTypeChange(e, type.types)}
-                                    />
-                                    <label
-                                        className="form-check-label"
-                                        htmlFor={key}>
-                                        {type.label}
-                                    </label>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
+					return (
+						<label className={styles.filterLabel} key={key}>
+							<input
+								type="radio"
+								name="type"
+								className={styles.filterRadio}
+								checked={isEqual(type.types, searchTypes)}
+								onChange={(e) => this.handleTypeChange(e, type.types)}
+							/>
+							{type.label}
+						</label>
+					);
+				})}
             </div>
         );
     }
@@ -112,35 +104,26 @@ class Fields extends React.Component<Props, State> {
             id: datasource.id
         })));
 
-        return (
-            <div className={'row ' + styles.filter}>
-                <div className="col-xs-12">
-                    <div className={styles.filterContent}>
-                        {options.map(option=> {
-                            const key = 'datasources_' + option.id;
+		return (
+			<div className={styles.filter}>
+				{options.map(option => {
+					const key = 'datasources_' + option.id;
 
-                            return (
-                                <div className="form-check form-check-inline" key={key}>
-                                    <input
-                                        type="radio"
-                                        className="form-check-input"
-                                        name="datasource"
-                                        id={key}
-                                        checked={option.id === datasourceFilter}
-                                        onChange={() => this.handleDatasourceChange(option.id)}
-                                    />
-                                    <label
-                                        className="form-check-label"
-                                        htmlFor={key}>
-                                        {option.label}
-                                    </label>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
-        );
+					return (
+						<label className={styles.filterLabel} key={key}>
+							<input
+								type="radio"
+								name="datasource"
+								className={styles.filterRadio}
+								defaultChecked={option.id === datasourceFilter}
+								onChange={() => this.handleDatasourceChange(option.id)}
+							/>
+							{option.label}
+						</label>
+					);
+				})}
+			</div>
+		);
     }
 
     render() {
@@ -149,30 +132,26 @@ class Fields extends React.Component<Props, State> {
 
         return (
             <div>
-				<h2>
-					<FormattedMessage id="fields"/>
-                </h2>
+				<h2><FormattedMessage id="fields"/></h2>
 
-				<div className="form-group">
-					<form>
-						<div className="row">
-							<div className="col-xs-12">
-								<input
-									className="form-control searchInput"
-									ref={searchInput => this.searchInput = searchInput}
-									value={this.state.query}
-									onChange={this.handleFieldSearchChange.bind(this)} type="text"
-									placeholder={intl.formatMessage({ id: 'search_fields' })} />
-							</div>
-						</div>
-						<div className={styles.filters}>
-							{this.renderTypeFilter()}
-							{this.renderDatasourceFilter()}
-						</div>
-					</form>
+				<form className={styles.form}>
+					<div className={styles.inputWrapper}>
+						<input
+							className={styles.input}
+							ref={searchInput => this.searchInput = searchInput}
+							value={this.state.query}
+							onChange={this.handleFieldSearchChange.bind(this)} type="text"
+							placeholder={intl.formatMessage({ id: 'search_fields' })}
+						/>
+						<Icon name={'ion-ios-search ' + styles.searchIcon} />
+					</div>
+					<div className={styles.filters}>
+						{this.renderTypeFilter()}
+						{this.renderDatasourceFilter()}
+					</div>
+				</form>
 
-                	<FieldList query={query} types={searchTypes} datasourceId={datasourceFilter}/>
-				</div>
+				<FieldList query={query} types={searchTypes} datasourceId={datasourceFilter}/>
             </div>
         );
     }
